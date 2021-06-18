397e7ae4659e787fd29e14faec4b2b4cc5ae9a67
renbz <1362206371@qq.com>
2021-06-18 14:55:24 +0800

    6.18

---
+++ src/main/java/com/ren/R02MyBlogItemApplication.java
@@ -0,0 +1,13 @@
+package com.ren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R02MyBlogItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R02MyBlogItemApplication.class, args);
+    }
+
+}
